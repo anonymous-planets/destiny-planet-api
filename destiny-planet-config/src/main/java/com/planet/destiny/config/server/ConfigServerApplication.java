2678374e63e16a502a8ea88b51efb88514af76e3
anonymous7planet <anonymous7planet@gmail.com>
2022-11-27 12:50:39 +0900

    Config 서버 기본 설정

---
+++ destiny-planet-config/src/main/java/com/planet/destiny/config/server/ConfigServerApplication.java
@@ -0,0 +1,27 @@
+package com.planet.destiny.config.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    /**
+     * endpoint 예시
+     * GET /{application}/{profile}[/{label}]
+     * GET /{application}-{profile}.yml
+     * GET /{label}/{application}-{profile}.yml
+     * GET /{application}-{profile}.properties
+     * GET /{label}/{application}-{profile}.properties
+     *
+     * application : 애플리케이션 이름
+     * profile : 환경
+     * label : git저장소의 브랜치 정보(기본값 : master)
+     * @param args
+     */
+    public static void main(String args[]) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
