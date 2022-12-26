4a2de6b1a65d76967eaaa960a5bc2dd4ef09891f
anonymous7planet <anonymous7planet@gmail.com>
2022-12-26 18:55:59 +0900

    CORS 설정 추가

--- destiny-planet-core-api/src/main/java/com/planet/destiny/core/api/config/WebConfiguration.java
+++ destiny-planet-core-api/src/main/java/com/planet/destiny/core/api/config/WebConfiguration.java
@@ -1,4 +1,35 @@
 package com.planet.destiny.core.api.config;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Slf4j
+@Configuration
 public class WebConfiguration {
+
+
+    /**
+     * 전역 공통 Cors 설정
+     * @return
+     */
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return configuration;
+    }
 }
